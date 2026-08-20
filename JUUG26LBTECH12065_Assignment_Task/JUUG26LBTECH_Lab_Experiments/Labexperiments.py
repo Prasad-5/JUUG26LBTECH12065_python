d81e45dc3a6e55328f0e5477589353e28294096c
Lakshmi Prasad <prasadroyal723@gmail.com>
2026-08-20 09:41:18 +0530

    Create Labexperiments.py

---
+++ JUUG26LBTECH12065_Assignment_Task/JUUG26LBTECH_Lab_Experiments/Labexperiments.py
@@ -0,0 +1,62 @@
+#Lab Experiment – 1
+#Name : A Lakshmi prasad
+#USN : 
+#Course : Python programming
+#Branch : AIDE – A
+#Date : 31/07/2026
+
+Lab Exercise 1: Installing Python, Setting up VS Code / PyCharm, Jupyter Notebook
+Aim
+To install Python and configure VS Code for Python programming.
+Steps
+1.	Download Python from python.org. 
+2.	Run the installer and check Add Python to PATH. 
+3.	Click Install Now and finish the installation. 
+4.	Open Command Prompt and verify:
+ 
+5.	Download VS Code from code.visualstudio.com. 
+6.	 Install VS Code and select Add to PATH. 
+7.	Open VS Code and install the Python extension. 
+8.	Create a new file named hello.py. 
+9.	Type:
+ 
+         print("Hello, World!")
+10.	Open the terminal and run:
+ 
+
+Lab Exercise 2: Write and execute a basic Python                   program.  
+Code :
+ print("im prasad")
+print("we are in python lab")
+
+
+
+
+Lab Exercise 3: Programs using variables, operators, and input/output.
+
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = a + b
+e = a * b
+print (c)
+print (e)
+ 
+Lab Exercise 4: Simple calculator and area calculation programs.  
+
+s = int(input("side of a square "))
+area = s * s
+print ("area of square is:", area)
+ 
+
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = input("enter the operation you want to perform ")
+if c == "add":
+    print(a + b)
+if c == "sub":
+    print(a - b)
+elif c == "mul":
+    print(a * b)
+else :
+    print(a / b)
+ 
